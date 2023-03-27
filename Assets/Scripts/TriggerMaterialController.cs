5057782fec766d895437c039678b3f54ac680d20
CosmicNyx <54959623+CosmicNyx@users.noreply.github.com>
2023-03-27 13:29:55 -0400

    objects dissolve once u step in collider

---
+++ Assets/Scripts/TriggerMaterialController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class TriggerMaterialController : MonoBehaviour
+{
+    public Material targetMaterial;
+    public float inputIncrement = 0.1f;
+
+    private bool playerEntered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerEntered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerEntered = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (playerEntered)
+        {
+            float currentInput = targetMaterial.GetFloat("_SurfaceInput");
+            float newInput = currentInput + inputIncrement * Time.deltaTime;
+            targetMaterial.SetFloat("_SurfaceInput", newInput);
+        }
+    }
+}
